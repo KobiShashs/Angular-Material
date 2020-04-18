e48b1ab1f29b929d4765ed615a8b0ad6455dc3d9
Kobi <kobi.shasha@gmail.com>
2020-04-19 00:35:13 +0300

    Material Buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { from } from 'rxjs';
+import { MaterialModule } from './material/material.module';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatIconModule],
   providers: [],
   bootstrap: [AppComponent]
 })
